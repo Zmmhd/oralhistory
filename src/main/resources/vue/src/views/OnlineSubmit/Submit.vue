afed35d85f0a4c2ba3e27884400fed488a711363
Busy <1422198347@qq.com>
2022-05-12 20:13:14 +0800

    busy 2022/5/12 20:13

--- src/main/resources/vue/src/views/OnlineSubmit/Submit.vue
+++ src/main/resources/vue/src/views/OnlineSubmit/Submit.vue
@@ -1,10 +1,29 @@
 <template>
-提交
+  <el-upload
+      drag
+      action="http://localhost:8888/review/upload"
+      multiple
+  >
+    <el-icon class="el-icon--upload"><UploadFilled /></el-icon>
+    <div class="el-upload__text">
+      Drop file here or <em>click to upload</em>
+    </div>
+    <template #tip>
+      <div class="el-upload__tip">
+        jpg/png files with a size less than 500kb
+      </div>
+    </template>
+  </el-upload>
 </template>
 
 <script>
+import { UploadFilled } from '@element-plus/icons-vue'
+
 export default {
-  name: "Submit"
+  name: "Submit",
+  components: {
+    UploadFilled
+  }
 }
 </script>
 
