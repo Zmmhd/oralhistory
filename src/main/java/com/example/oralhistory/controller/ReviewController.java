0e2e5a3691a791a7734bb6687e3d2918b61b6a76
Busy <1422198347@qq.com>
2022-05-15 10:55:56 +0800

    busy 2022/5/15 10:55

--- src/main/java/com/example/oralhistory/controller/ReviewController.java
+++ src/main/java/com/example/oralhistory/controller/ReviewController.java
@@ -26,8 +26,9 @@ public class ReviewController {
 
     /**
      * 通过状态查询审核
-     * @param status 状态
-     * @param pageNum 第几页
+     *
+     * @param status   状态
+     * @param pageNum  第几页
      * @param pageSize 页面大小
      * @return 响应
      */
@@ -55,9 +56,10 @@ public class ReviewController {
 
     /**
      * 根据手机号进行审核查询
+     *
      * @param upernumber 手机号
-     * @param pageNum 第几页
-     * @param pageSize 页面大小
+     * @param pageNum    第几页
+     * @param pageSize   页面大小
      * @return 响应
      */
     @GetMapping("/upernumberlike/{upernumber}")
@@ -70,7 +72,8 @@ public class ReviewController {
     /**
      * 修改审核，即选择通过或者不通过,
      * 修改实体review的status.1为通过；-1为不通过
-     * @param id 审核的id
+     *
+     * @param id     审核的id
      * @param status 审核建议 1为通过；-1为不通过
      * @return
      */
@@ -78,34 +81,50 @@ public class ReviewController {
     public ResponseEntity update(@PathVariable Integer id,
                                  @RequestParam int status,
                                  HttpServletRequest request) {
-        if(status == 1){
+        if (status == 1) {
             return reviewService.passReview(id);
-        }else if(status == -1){
-            return  reviewService.failReview(id,request);
+        } else if (status == -1) {
+            return reviewService.failReview(id, request);
         }
-        return RespondResult.error("失败",400);
+        return RespondResult.error("失败", 400);
 
     }
 
     /**
      * 上传文件与新建一个审核和资源
      * 注意传过来的两个实体要满足下列条件
+     *
      * @param multipartFile 用户上传的文件
-     * @param resource 不需要字段id、status、uptime、url、
-     * @param review 不需要的字段id、status、uptime、resourceid、
+     * @param resource      不需要字段id、status、uptime、url、
+     * @param review        不需要的字段id、status、uptime、resourceid、
      * @return
      */
+//    @RequestMapping("/upload")
+//    public ResponseEntity upload(@RequestParam("multipartFile") MultipartFile multipartFile,
+//                                 @RequestParam("resource") String resource,
+//                                 @RequestParam("review") String review,
+//                                 HttpServletRequest request) {
+//        System.out.println("dDADAAFF");
+////        resource.setStatus(0);
+////        review.setStatus(0);
+////        resource.setUptime(LocalDate.now());
+////        resource.setUptime(LocalDate.now());
+////        return reviewService.uploadFile(multipartFile, review, resource, request);
+//        return RespondResult.error("失败", 400);
+//    }
+
     @PostMapping(value = "/upload",produces = "application/json;charset=utf-8")
-    public ResponseEntity upload(@RequestParam MultipartFile multipartFile,
-                                 @RequestParam Resource resource,
-                                 @RequestParam Review review,
+    public ResponseEntity upload(@RequestParam("multipartFile") MultipartFile multipartFile,
+                                 @RequestParam("resource") String resource,
+                                 @RequestParam("review") String review,
                                  HttpServletRequest request){
+//        System.out.println("sdsfgdasfdvfvfv");
         resource.setStatus(0);
         review.setStatus(0);
         resource.setUptime(LocalDate.now());
         resource.setUptime(LocalDate.now());
         return reviewService.uploadFile(multipartFile, review, resource,request);
+//        return RespondResult.error("失败lll", 555);
     }
-
 
 }
--- src/main/resources/vue/src/views/OnlineSubmit/Submit.vue
+++ src/main/resources/vue/src/views/OnlineSubmit/Submit.vue
@@ -1,28 +1,95 @@
 <template>
-  <el-upload
-      drag
-      action="http://localhost:8888/review/upload"
-      multiple
-  >
-    <el-icon class="el-icon--upload"><UploadFilled /></el-icon>
-    <div class="el-upload__text">
-      Drop file here or <em>click to upload</em>
-    </div>
-    <template #tip>
-      <div class="el-upload__tip">
-        jpg/png files with a size less than 500kb
-      </div>
-    </template>
-  </el-upload>
+  <el-form :model="form">
+    <el-form-item label="资源类型">
+      <el-input v-model="form.type"/>
+    </el-form-item>
+    <el-form-item label="标题">
+      <el-input v-model="form.title"/>
+    </el-form-item>
+    <el-form-item label="资源简介">
+      <el-input v-model="form.synopsis"/>
+    </el-form-item>
+    <el-form-item label="省份">
+      <el-input v-model="form.province"/>
+    </el-form-item>
+    <el-form-item label="档案馆名称">
+      <el-input v-model="form.archives"/>
+    </el-form-item>
+    <el-form-item label="主题曲">
+      <el-input v-model="form.theme"/>
+    </el-form-item>
+    <el-form-item label="上传者城市">
+      <el-input v-model="form.upercity"/>
+    </el-form-item>
+    <el-form-item label="上传者姓名">
+      <el-input v-model="form.upername"/>
+    </el-form-item>
+    <el-form-item label="上传者手机号">
+      <el-input v-model="form.upernumber"/>
+    </el-form-item>
+    <el-form-item label="文件上传">
+      <el-upload action="" :auto-upload="false" ref="uploadRef" :http-request="uploadFile">
+        <el-button type="primary">Click to upload</el-button>
+        <template #tip>
+          <div>
+            jpg/png files with a size less than 500KB.
+          </div>
+        </template>
+      </el-upload>
+    </el-form-item>
+    <el-button type="primary" @click="save">确 定</el-button>
+  </el-form>
 </template>
 
 <script>
-import { UploadFilled } from '@element-plus/icons-vue'
+import {UploadFilled} from '@element-plus/icons-vue'
+import request from "@/utils/request";
 
 export default {
   name: "Submit",
   components: {
     UploadFilled
+  },
+  data() {
+    return {
+      form: {},
+    }
+  },
+  created() {
+    this.myFile = new FormData();
+  },
+  methods: {
+    // 自定义上传
+    uploadFile(params) {
+      const fileObj = params.file;
+      let data = new FormData();
+      data.append("multipartFile", fileObj);
+      let resource = {
+        "type": this.form.type,
+        "title": this.form.title,
+        "synopsis": this.form.synopsis,
+        "province": this.form.province,
+        "archives": this.form.archives,
+        "theme": this.form.theme,
+        "upercity": this.form.upercity,
+        "upername": this.form.upername,
+        "upernumber": this.form.upernumber
+      };
+      data.append('resource', JSON.stringify(resource));
+      let review = {
+        "title": this.form.title,
+        "synopsis": this.form.synopsis,
+        "upernumber": this.form.upernumber
+      };
+      data.append('review', JSON.stringify(review));
+      console.log("fsdfdsfsfdsfsdfd")
+      request.post("/review/upload", data).then(res => {
+        console.log(res);
+      })
+    },
+    save() {
+      this.$refs.uploadRef.submit(); //触发自定义上传
+    }
   }
 }
 </script>
